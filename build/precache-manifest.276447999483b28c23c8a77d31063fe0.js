f2bc7543a20da6beab7b8ffd3f5c5165add67d74
rwik <rwikee@gmail.com>
2020-09-06 20:15:53 +0530

    Directory restructured

---
+++ build/precache-manifest.276447999483b28c23c8a77d31063fe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff287c272ecb59582c8481235c91707d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d5a643cdc8f51a694f",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "daa86adf14e71bdb371f",
+    "url": "/static/js/2.411077b7.chunk.js"
+  },
+  {
+    "revision": "a7d5a643cdc8f51a694f",
+    "url": "/static/js/main.551ddaf0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
