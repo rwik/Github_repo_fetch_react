41515eb065844d2c804d5d276d2946c04c07220e
rwik <rwikee@gmail.com>
2020-09-06 19:40:07 +0530

    Initial commit

---
+++ github-fetch/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <p>Project made following tutorial from travis media </p>
+      <p>App to search Github users</p>
+      <a href="https://rwik.github.io/blog/"> Rwik's blog</a>
+    </Fragment>
+  );
+};
+
+export default About;
